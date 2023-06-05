820e5270e54661bfec7b70a737b0f1c5ea7ef987
Ivanova Anna <ai4832722@gmail.com>
2023-06-05 14:10:29 +0700

    Вывод даты

---
+++ lab8.js
@@ -0,0 +1,10 @@
+function showDate() {
+    let out = document.getElementById('current-date');
+    let today = new Date();
+    out.innerHTML = "Дата и время для русской локали: "+ today.toLocaleString('ru-RU') + '<br>' +
+    "Дата и время для бразильской локали: "+ today.toLocaleString('be') + '<br>' +
+    "Дата и время для австрийской локали: "+ today.toLocaleString('de-AT') + '<br>' +
+    "Дата и время для канадской локали: "+ today.toLocaleString('en-CA') + '<br>' +
+    "Дата и время для итальянской локали: "+ today.toLocaleString('it-IT') + '<br>';
+    out.style.color = 'navy';
+}
